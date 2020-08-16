bf743f3da64d3323bc8e1c67d3e84ed8d31fe013
elkasa <69632469+elkasa@users.noreply.github.com>
2020-08-16 19:49:13 +0200

    Update myw1.py

--- myw1.py
+++ myw1.py
@@ -1,11 +1,19 @@
-import os
-import zipfile
+import shutil
 import random
-out_filename = ".//out.txt"
-filename = ".//mytst2.txt"
-directoryIn = './/in//'
-directoryOut = './/out//'
+import logging
+import pathlib 
 
+
+
+current_dir=pathlib.Path.cwd()
+logging.basicConfig(filename='logfile.log',level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+
+dirpath = pathlib.Path.cwd()/'in'
+dirout=pathlib.Path.cwd()/'out'
+dirrejet=pathlib.Path.cwd()/'rejet'
+assert(dirpath.is_dir())
+assert(dirout.is_dir())
+assert(dirrejet.is_dir())
 
 def validate(ae,em,bf,res):
     if len(bf) == 2 :
@@ -29,14 +37,49 @@ def validate(ae,em,bf,res):
         pass
     return res
 
-    
-    
-    
+
+
+def pre_process(path,filename,dirrejet):
+    with open(filename) as f:
+            lines=f.readlines()
+            AE=[item for item in lines if item[0:2] == "AE"]
+            EM=[item for item in lines if item[0:2] == "EM"]
+            BF=[item for item in lines if item[0:2] == "BF"]
+            EN=[item for item in lines if item[0:2] == "EN"]
+            
+#             if AE.split(";")[3] == "":
+#                 logging.error('This file rejected KO raison AE ID not set : ' + fname)
+#                 shutil.move(filename, dirrejet/fname)               
+#                 zipfile=(fname.split(".")[0])+'.zip'
+#                 fl=pathlib.Path(path/zipfile)
+#                 if fl.is_file():
+#                     shutil.move(fl, dirrejet/zipfile)
+#                     logging.error('This file rejected KO : ' +  zipfile)
+#                 return False
+#             else: 
+#                 continue
+
+            if len(AE) == 0 or len(EM) == 0 or len(BF) == 0 or len(EN) ==0 :
+                fname=filename.name
+                logging.error('This file rejected KO raison AE/EM/BF/EN not set : ' + fname)
+                shutil.move(filename, dirrejet/fname)
+                
+                zipfile=(fname.split(".")[0])+'.zip'
+                fl=pathlib.Path(path/zipfile)
+                if fl.is_file():
+                    shutil.move(fl, dirrejet/zipfile)
+                    logging.error('This file rejected KO : ' +  zipfile)
+                return False
+            else:
+                logging.info('Pre_processing OK for file : ' + filename.name)
+                return True
+                
+
 def process(path,filename,outpath):
     header=[]
     footer=[]
-    out_filename =  outpath + filename+".out"
-    filename= path + filename
+    out_filename =  outpath/filename.name
+    
 
     bf=[]
     dn=[]
@@ -70,11 +113,14 @@ def process(path,filename,outpath):
                 if line[0:2] == "ES":
                     es=line.strip()
                     les= es.split(";")
-                    if les[10] == "":
-                        y=round(random.uniform(0.6,1.2),2)
-                        les[10]=str(y)
-                    print(';'.join(les))
-                    footer.append(';'.join(les).strip())
+                    if les[2] =="S":
+                        footer.append(es)
+                    else:
+                        if les[10] == "":
+                            y=round(random.uniform(0.6,1.2),2)
+                            les[10]=str(y)
+                            footer.append(';'.join(les).strip())
+                    #footer.append(es)
                 if line[0:2] == "EN":
                     i_en +=1
                     if len(bf) > 0:
@@ -94,10 +140,26 @@ def process(path,filename,outpath):
             out_file.write('%s\n' % dn[i])
         for i in range (0,len(footer)):
             out_file.write('%s\n' % footer[i])
-for filename in os.listdir(directoryIn):
-    if filename.endswith(".zip"):
-        with zipfile.ZipFile(directoryIn+filename, 'r') as myzip:
-            myzip.extractall(directoryIn)
-    if filename.endswith(".txt"):
-        print(filename)
-        process(directoryIn,filename,directoryOut)    
+
+
+
+logging.info('Start files processing  directory input : '+ dirpath.name)
+logging.info('Start files processing  directory output : '+ dirout.name)
+i_zip = 0
+i_txt = 0
+for f in dirpath.iterdir():
+    if (f.name).endswith(".zip"):
+        i_zip +=1
+        logging.info('unzip file  : '+ f.name)
+        shutil.unpack_archive(f,dirpath)
+for f in dirpath.iterdir():
+    if (f.name).endswith(".txt"):
+        i_txt += 1
+        logging.info('pre_process file ===>  : '+ f.name)
+        res = pre_process(dirpath,f,dirrejet) 
+        if res == True :
+            process(dirpath,f,dirout)
+        else:
+            continue
+
+logging.info('End processing of : ' + str(i_zip) + ' zip files to :'+ str(i_txt) + ' txt files')
